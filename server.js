186e8f9c2480af5b036fce548fd592dc1199b729
Lala Mammadova <mammadvalala@gmail.com>
2026-05-17 23:36:10 +0400

    Refactor server.js to use product routes

--- server.js
+++ server.js
@@ -1,37 +1,13 @@
 const express = require("express");
-const db = require("./db.js");
 
 const app = express();
-const PORT = 3000;
+
+const productRoutes = require("./routes/productRoutes");
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ 
-    message: "✅ E-commerce Backend Server işləyir!",
-    status: "OK"
-  });
-});
+app.use("/products", productRoutes);
 
-app.get("/test-db", (req, res) => {
-  db.query("SELECT 1 + 1 as result", (err, results) => {
-    if (err) {
-      return res.status(500).json({ error: "DB xətası: " + err.message });
-    }
-    res.json({ message: "✅ DB işləyir!", result: results[0] });
-  });
-});
-
-app.listen(PORT, () => {
-  console.log('🌐 Server http://localhost:${PORT} da işləyir');
-});
-app.get("/products", (req, res) => {
-  db.query("SELECT * FROM products", (err, results) => {
-    if (err) {
-      res.status(500).send(err.message);
-      return;
-    }
-
-    res.json(results);
-  });
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
